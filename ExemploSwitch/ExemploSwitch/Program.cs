13f53d5585c5f81b21a474e4f4c4781d0f6651e7
Gb-codertech <gbvieira2007@gmail.com>
2024-11-29 12:58:58 +0000

    Add files via upload

---
+++ ExemploSwitch/ExemploSwitch/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+public class ExemploSwitch
+{
+    public static void Main(string[] args)
+    {
+        int grade = 12;
+        // E = && , OU = ||
+        switch (grade)
+        {
+            case >= 18 and <= 20:
+                Console.WriteLine("Excelent");
+                break;
+
+            case >= 14 and <= 17:
+                Console.WriteLine("Good");
+                break;
+
+            case >= 10 and <= 13:
+                Console.WriteLine("Enought");
+                break;
+
+            case >= 0 and <= 9:
+                Console.WriteLine("Not Enought");
+                break;
+
+            default:
+                Console.WriteLine("Nota inválida");
+                break;
+        }
+    }
+}
